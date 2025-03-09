04a89361803d62795a1a157cfdd70008e5ad502c
rextivlan <affan.ali368@gmail.com>
2025-03-09 15:56:12 -0400

    Add Controller and Dependencies

---
+++ expensetrackerapi/src/main/java/com/ali/expensetrackerapi/controller/ExpenseController.java
@@ -0,0 +1,13 @@
+package com.ali.expensetrackerapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ExpenseController {
+	
+	@GetMapping("/expenses")
+	public String getAllExpenses() {
+		return "List of Expenses";
+	}
+}
